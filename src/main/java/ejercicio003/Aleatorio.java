98bfb7a23364162f71a741d8b9a9b0093625f40f
pablo.herrefu <usuario@L-2103086.cic.es>
2021-07-12 23:10:06 +0200

    primer commit ejercicio003

---
+++ src/main/java/ejercicio003/Aleatorio.java
@@ -0,0 +1,14 @@
+package ejercicio003;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Aleatorio {
+	Map<String, Integer> people = new HashMap<>();
+	
+	people.put("Alvaro", 1);
+	people.put("Jaime", 2);
+	people.put("Laura", 3);
+	people.put("Lucia", 4);
+
+}
