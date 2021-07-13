6e4f8796b41dc950629d7d9bac632090cfcce17f
pablo.herrefu <usuario@L-2103086.cic.es>
2021-07-13 08:40:41 +0200

    actualizado gitignore

--- src/main/java/ejercicio003/Aleatorio.java
+++ src/main/java/ejercicio003/Aleatorio.java
@@ -1,10 +1,24 @@
 package ejercicio003;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Aleatorio {
-	Map<String, Integer> people = new HashMap();
+	ArrayList<String> personas = new ArrayList<String>();
+	
+	personas.add("Jesús");
+	personas.add("Lucía");
+	personas.add("Manuel");
+	personas.add("Jose");
+	personas.add("Javi");
+	
+	size = personas.size();
+	
+	private int aleatorio() {
+		int x = random.nextInt(size);
+		return lista.get(x);
+	}
+	
 	
 
 
---
+++ src/test/java/ejercicio003/AleatorioTest.java
@@ -0,0 +1,10 @@
+package ejercicio003;
+
+import statuc org.junit.jupiter.Assertions.*
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AleatorioTest extends Aleatorio {
+	
+}
